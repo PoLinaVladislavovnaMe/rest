1fb3479ca89d86389499819684639f1185656190
Polina Moiseenko <trylyalya@rambler.ru>
2023-12-14 17:46:45 +0700

    Tests changed

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,4 +1,4 @@
- package ru.netology.rest;
+package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
 
@@ -7,15 +7,15 @@ import static io.restassured.RestAssured.given;
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200);
     }
 }
